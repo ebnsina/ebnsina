ace862a03a94962a71059d551d66ecfa3fdc29a5
Ebn Sina <ebnsina@amharctech.com>
2025-08-01 13:58:14 +0600

    add PatternedSection

---
+++ src/lib/components/shared/PatternedSection.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let title: string;
+	export let description: string;
+</script>
+
+<div
+	class="py-20 border-b border-slate-200 px-6"
+	style="
+    background-image: 
+      repeating-linear-gradient(0deg, rgba(0,0,0,0.05) 0px, rgba(0,0,0,0.05) 1px, transparent 1px, transparent 14px),
+      repeating-linear-gradient(90deg, rgba(0,0,0,0.05) 0px, rgba(0,0,0,0.05) 1px, transparent 1px, transparent 14px);
+  "
+>
+	<h1 class="text-xl md:text-3xl xl:text-4xl font-bold mb-4">{title}</h1>
+	<p class="text-sm md:text-base text-slate-600">{description}</p>
+</div>
--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import BlogPost from '$lib/components/blog/BlogPost.svelte';
 	import BlogTag from '$lib/components/blog/BlogTag.svelte';
+	import PatternedSection from '$lib/components/shared/PatternedSection.svelte';
 	import { tagsColors } from '$lib/utils/constants';
 	import { flip } from 'svelte/animate';
 
@@ -13,19 +14,10 @@
 </svelte:head>
 
 <section class="min-h-screen flex justify-center flex-col">
-	<div
-		class="py-20 border-b border-slate-200 px-6"
-		style="
-    background-image: 
-      repeating-linear-gradient(0deg, rgba(0,0,0,0.05) 0px, rgba(0,0,0,0.05) 1px, transparent 1px, transparent 14px),
-      repeating-linear-gradient(90deg, rgba(0,0,0,0.05) 0px, rgba(0,0,0,0.05) 1px, transparent 1px, transparent 14px);
-  "
-	>
-		<h1 class="text-xl md:text-3xl xl:text-4xl font-bold mb-4">Blogs</h1>
-		<p class="text-sm md:text-base text-slate-600">
-			Deep dives and practical tips on development and technology.
-		</p>
-	</div>
+	<PatternedSection
+		title="Blogs"
+		description="Deep dives and practical tips on development and technology."
+	/>
 
 	<div class="px-6 py-10 border-b border-slate-200">
 		<div class="flex gap-2.5 items-center flex-wrap">
